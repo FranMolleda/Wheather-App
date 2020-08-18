600b90f7dfe613be2f1df2ff9a9b3de8a41b830e
Fran <fmjerez@icloud.com>
2020-08-18 10:56:58 +0200

    Validation and query data done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
 function App() {
+  const [search, setSearch] = useState({
+    city: "",
+    country: "",
+  });
+
+  const [query, setQuery] = useState(false);
+
+  const { city, country } = search;
+
+  useEffect(() => {
+    if (query) {
+      console.log(city, country);
+    }
+    setQuery(false);
+  }, [query]);
+
+  console.log(search);
   return (
     <Fragment>
       <Header title="Clima React app" />
@@ -9,7 +26,7 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col m6 s12">
-              <Form />
+              <Form search={search} setSearch={setSearch} setQuery={setQuery} />
             </div>
             <div className="col m6 s12">2</div>
           </div>
